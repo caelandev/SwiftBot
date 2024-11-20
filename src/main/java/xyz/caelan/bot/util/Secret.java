70c99f5ac601b1f7e0ded2bd8fa4601e0ddf0c54
Caelan <146661112+caelandev@users.noreply.github.com>
2024-11-21 00:30:00 +0500

    Update Secret.java

--- src/main/java/xyz/caelan/bot/util/Secret.java
+++ src/main/java/xyz/caelan/bot/util/Secret.java
@@ -1,7 +1,7 @@
 package xyz.caelan.bot.util;
 
 public class Secret {
-    public static final String token = "MTMwMjMxNzc5MTU2NjYzMTAzMw.GN7p1y.Z_vIScx6whAy6VAVv32RFdM27DPuzP2R8oosiA";
+    public static final String token = "MTMwMjMxNzc5MTU2NjYzMTAzMw.GN7p1y.Z_vIScx6whAy6VAVv32RFdM27DPuzP2R8oosiA"; // your token here
 
     public static String getToken() {
         return token;
